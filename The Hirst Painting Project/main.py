73424a2b1dd1c7ab64aaf6a65ddee7240c16ac52
tjkreddy <thangellajugalkishore@gmail.com>
2024-03-07 10:17:57 +0530

    turtle graphics

---
+++ The Hirst Painting Project/main.py
@@ -0,0 +1,44 @@
+from turtle import Turtle, Screen
+import random
+tim = Turtle()
+tim.shape("turtle")
+tim.color("red")
+
+
+'''draw a square'''
+#
+# for _ in range(4):
+#     tim.right(90)
+#     tim.forward(100)
+
+'''print hero names'''
+
+# import heroes
+# print(heroes.gen())
+
+'''print a dashed line'''
+
+# for _ in range(10):
+#     tim.penup()
+#     tim.forward(10)
+#     tim.pendown()
+#     tim.forward(10)
+#
+
+'''draw a triangle, square, pentagon, hexagon, heptagon, octagon, nonagon and decagon'''
+
+#
+# color_list = ["red", "blue", "green", "black"]
+#
+# for num in range(3, 11):
+#     angle = 360/num
+#     color = random.choice(color_list)
+#     tim.pencolor(color)
+#     for _ in range(num):
+#         tim.forward(100)
+#         tim.right(angle)
+#
+# screen = Screen()
+# screen.exitonclick()
+#
+
